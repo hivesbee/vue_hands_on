e591c5386901ae7254dc152184d22ce0062f6e79
hivesbee <hivesbee@gmail.com>
2018-03-13 16:06:49 +0900

    init

---
+++ ep4/server/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
