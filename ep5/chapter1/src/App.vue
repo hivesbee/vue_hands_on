f08f556069d4abd6f202e14cbbdef2296c947a29
hivesbee <hivesbee@gmail.com>
2018-08-09 03:40:44 +0900

    ep5 資料作成

---
+++ ep5/chapter1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Contact List</h1>
+    <input type="text" v-model="newContact">
+    <button type="button" @click="addContact()">add contact</button>
+    <ul>
+      <li v-for="(contact, i) in contacts">
+        <span>{{ contact }}</span><button type="button" @click="removeContact(i)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      newContact: '',
+      contacts: []
+    }
+  },
+  created () {
+    this.contacts.push('cloud taro')
+    this.contacts.push('cloud jiro')
+    this.contacts.push('cloud saburo')
+  },
+  methods: {
+    addContact: function() {
+      this.contacts.push(this.newContact);
+      this.newContact = '';
+    },
+    removeContact: function(i) {
+      this.contacts.splice(i, 1);
+    }
+  }
+}
+</script>
---
+++ ep5/chapter1/src_answer/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>Contact List</h1>
+    <contact-input v-model.trim="newContact" @add="addContact()"></contact-input>
+    <contact-item v-for="(contact, i) in contacts" :key="i" :contact="contact" @remove="removeContact(i)"></contact-item>
+  </div>
+</template>
+
+<script>
+import ContactInput from './components/ContactInput.vue'
+import ContactItem from './components/ContactItem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ContactInput,
+    ContactItem
+  },
+  data () {
+    return {
+      newContact: '',
+      contacts: []
+    }
+  },
+  created () {
+    // ここで以下のように直接追加する代わりに fetch の API をたたいて、
+    // その結果を this.contacts に割り当てるとかにするとそれっぽいですよね
+    this.contacts.push('cloud taro')
+    this.contacts.push('cloud jiro')
+    this.contacts.push('cloud saburo')
+  },
+  methods: {
+    addContact () {
+      this.contacts.push(this.newContact)
+      this.newContact = ''
+    },
+    removeContact (i) {
+      this.contacts.splice(i, 1)
+    }
+  }
+}
+</script>
